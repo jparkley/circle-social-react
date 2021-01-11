1b430ad5165a04f225f6810d8f8d413add5091d6
Jjin SoL <parkleydondon@yahoo.com>
2021-01-10 22:06:52 -0800

    New account & posts creation, login/logout

--- app/Main.js
+++ app/Main.js
@@ -1,21 +1,42 @@
 import { func } from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import ReactDOM from "react-dom"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
+import Axios from "axios"
 
+import FlashMessages from "./components/FlashMessages"
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
 import About from "./components/About"
 import Terms from "./components/Terms"
+import Home from "./components/Home"
+import CreatePost from "./components/CreatePost"
+import ViewSinglePost from "./components/ViewSinglePost"
+
+Axios.defaults.baseURL = "http://localhost:8080"
 
 function Main() {
+  const [loggedIn, setLoggedIn] = useState(Boolean(localStorage.getItem("circleAppToken")))
+  const [flashMessages, setFlashMessages] = useState([])
+
+  function addFlashMessage(msg) {
+    setFlashMessages(prev => prev.concat(msg))
+  }
+
   return (
     <BrowserRouter>
-      <Header />
+      <FlashMessages messages={flashMessages} />
+      <Header loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
       <Switch>
         <Route path="/" exact>
-          <HomeGuest />
+          {loggedIn ? <Home /> : <HomeGuest />}
+        </Route>
+        <Route path="/create-post">
+          <CreatePost addFlashMessage={addFlashMessage} />
+        </Route>
+        <Route path="/post/:id">
+          <ViewSinglePost />
         </Route>
         <Route path="/about-us">
           <About />
