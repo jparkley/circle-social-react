c0c301e79f031e5fae2f7c9bde21b3558c0a53c4
Jjin SoL <parkleydondon@yahoo.com>
2021-01-15 23:37:28 -0800

    Edited home intro

--- app/components/HomeGuest.js
+++ app/components/HomeGuest.js
@@ -196,7 +196,7 @@ function HomeGuest() {
       <div className="row align-items-center">
         <div className="col-lg-7 py-3 py-md-5">
           <h1 className="display-3">Remember Writing?</h1>
-          <p className="lead text-muted">Are you sick of short tweets and impersonal &ldquo;shared&rdquo; posts that are reminiscent of the late 90&rsquo;s email forwards? We believe getting back to actually writing is the key to enjoying the internet again.</p>
+          <p className="lead text-muted">This is your Friendship Couch, where you'd like to share fresh ideas, non-opinionated thoughts, any riveting information and more. We're not random balls rolling around the concrete. Let's share and connect again. =)</p>
         </div>
         <div className="col-lg-5 pl-lg-5 pb-3 py-lg-5">
           <form onSubmit={handleSubmit}>
